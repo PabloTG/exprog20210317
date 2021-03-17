aa6f18fb168cb8ef5188dcfa01267bce39e9b2a1
alumno <alumno@192.168.59.1>
2021-03-17 18:35:46 +0100

    Ejercicio 3 en proceso (acepta una primera línea sin calcular)

---
+++ src/fp/daw/examen2ev/Calculadora.java
@@ -0,0 +1,52 @@
+package fp.daw.examen2ev;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Calculadora {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("calculadora > ");
+		String linea = in.readLine();
+		while (!(linea = in.readLine()).equalsIgnoreCase("fin")) {
+			calcular(linea);
+			System.out.print("calculadora > ");
+		} // fin while
+	} // fin main
+
+	static void calcular(String linea) {
+		// separo la línea introducida por teclado en sus componentes
+		// (que estarán separados por un espacio en blanco) y los
+		// almaceno en un array de cadenas de caracteres
+		String[] operacion = linea.split(" ");
+		// uso los componentes del array para tener los elementos de la
+		// operación a realizar (asumiendo que serán operando, operación,
+		// operando, como en los ejemplos del enunciado)
+		try {
+			double operando1 = Double.parseDouble(operacion[0]);
+			String operador = operacion[1];
+			double operando2 = Double.parseDouble(operacion[2]);
+			// realizo la operación y la muestro por pantalla
+			switch (operador) {
+			case "+":
+				System.out.println(operando1 + operando2);
+				break;
+			case "-":
+				System.out.println(operando1 - operando2);
+				break;
+			case "*":
+				System.out.println(operando1 * operando2);
+				break;
+			case "/":
+				System.out.println(operando1 / operando2);
+				break;
+			default:
+				throw new Exception();
+			} // fin switch
+		} // fin try
+		catch (Exception e) {
+			System.out.println("expresión incorrecta");
+		}
+	}
+}
